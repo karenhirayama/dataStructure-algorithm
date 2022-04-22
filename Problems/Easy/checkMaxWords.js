b906fad6a72dfe7f19e8664ae621ddedadce18ab
Karen Mayumi Hirayama <karenm.hirayama@gmail.com>
2022-04-21 22:41:38 -0300

    Remove comments

--- Problems/Easy/checkMaxWords.js
+++ Problems/Easy/checkMaxWords.js
@@ -1,12 +1,11 @@
 var mostWordsFound = function (sentences) {
-    // No complexity code
-    // let numSpaces = 0;
-    // for (let i in sentences) {
-    //     if (sentences[i].split(/\s/).length >= numSpaces) {
-    //         numSpaces = sentences[i].split(/\s/).length;
-    //     }
-    // }
-    // return numSpaces;
+    let numSpaces = 0;
+    for (let i in sentences) {
+        if (sentences[i].split(/\s/).length >= numSpaces) {
+            numSpaces = sentences[i].split(/\s/).length;
+        }
+    }
+    return numSpaces
 };
 
 console.log(mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]));
